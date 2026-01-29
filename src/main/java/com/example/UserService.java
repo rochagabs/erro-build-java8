b3c9d1b918c706e8a0ce0213a546e88ffcca924e
Gabriela <gabirochasf@gmail.com>
2026-01-29 09:29:53 -0300

    Modernize Java syntax to Java 21 features

--- src/main/java/com/example/UserService.java
+++ src/main/java/com/example/UserService.java
@@ -5,14 +5,8 @@ import java.util.List;
 public class UserService {
 
     public void printAdultUsers(List<User> users) {
-
-        // ERRO: inferência inválida com 'var' em campo
-        var totalUsers = users.size();
-
-        for (User user : users) {
-            if (user.getAge() >= 18) {
-                System.out.println(user.getName() + " é adulto");
-            }
-        }
+        users.stream()
+                .filter(user -> user.age() >= 18)
+                .forEach(user -> System.out.println(user.name() + " é adulto"));
     }
 }
